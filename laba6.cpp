c243bb78a02f2ea6047c3968ef67e64e9bd3fb45
vladistud <life404found@gmail.com>
2025-02-25 14:04:52 +0500

    Update laba6.cpp

--- laba6.cpp
+++ laba6.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 
-void sortDigitsDescending(char* str) 
-    int count[10] = {0}; 
+void sortDigitsDescending(char* str) {
+    int count[10] = {0};
+
     for (char* ptr = str; *ptr; ++ptr) {
         if (*ptr >= '0' && *ptr <= '9') {
             count[*ptr - '0']++;
         }
-    
+    }
+
     for (char* ptr = str; *ptr; ++ptr) {
         if (*ptr >= '0' && *ptr <= '9') {
-            for (int i = 9; i >= 0; --i) { 
-                while (count[i] > 0) { 
+            for (int i = 9; i >= 0; --i) {
+                while (count[i] > 0) {
                     *ptr = '0' + i;
                     count[i]--;
-                    ++ptr;
+                    break;
                 }
             }
-            break;
         }
     }
 }
 
 int main() {
     char str[] = "abc1234def982gh";
-    
     std::cout << "Исходная строка: " << str << std::endl;
     sortDigitsDescending(str);
     std::cout << "После сортировки цифр по убыванию: " << str << std::endl;
-
     return 0;
 }
